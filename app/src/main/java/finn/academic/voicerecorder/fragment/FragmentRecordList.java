4aaf41f88134c90c0685ac648573961dfe1c42fd
hip-nncuong162 <63356009+hip-nncuong162@users.noreply.github.com>
2022-04-11 00:05:40 +0700

    structure for play UI

--- app/src/main/java/finn/academic/voicerecorder/fragment/FragmentRecordList.java
+++ app/src/main/java/finn/academic/voicerecorder/fragment/FragmentRecordList.java
@@ -15,6 +15,10 @@ public class FragmentRecordList extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_record_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_record_list, container, false);
+
+
+
+        return view;
     }
 }
