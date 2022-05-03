7e518f05189dccc916502db93ab94c2332ab58d7
Võ Trần Quang Thông <64117392+kaka0902669477@users.noreply.github.com>
2022-05-03 18:53:59 +0700

    add playlist in DeletedRecordActivity

--- app/src/main/java/finn/academic/voicerecorder/DeletedRecordsActivity.java
+++ app/src/main/java/finn/academic/voicerecorder/DeletedRecordsActivity.java
@@ -2,13 +2,20 @@ package finn.academic.voicerecorder;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.Image;
 import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -16,8 +23,10 @@ import android.view.animation.AnimationUtils;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -45,9 +54,15 @@ public class DeletedRecordsActivity extends AppCompatActivity implements RecordA
     private LinearLayout mainPlayLayout;
     private LinearLayout emptyAlert;
 
-    private TextView playHeading;;
+    private TextView playHeading, namePlayingRecord, durationPlayingRecord, startTimePlayingRecord, endTimePlayingRecord;
 
     private RelativeLayout utilLayout;
+
+    private ImageButton playRecord, rewindRecord, forwardRecord, deletePlayingRecord;
+    private SeekBar seekBarRecord;
+    private int pos;
+    static MediaPlayer mMediaPlayer;
+    SharedPreferences sharedPreferences;
 
     private ArrayList<File> files;
     private ArrayList<File> allFiles;
@@ -92,14 +107,69 @@ public class DeletedRecordsActivity extends AppCompatActivity implements RecordA
         deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showYesNoDialogForDelete();
+                showYesNoDialog();
             }
         });
 
         recoverBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showYesNoDialogForRecover();
+                if (recoverBtn.getText().equals(getResources().getString(R.string.recover_all))) {
+                    if (adapter.recoverAllRecords(allFiles, allRecords)) {
+                        Toast.makeText(view.getContext(), view.getContext()
+                                .getResources().getString(R.string.recover_all_alert), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(view.getContext(), view.getContext()
+                                .getResources().getString(R.string.error), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    if (adapter.recoverAllSelected(allFiles, allRecords)) {
+                        Toast.makeText(view.getContext(), view.getContext()
+                                .getResources().getString(R.string.recover_alert), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(view.getContext(), view.getContext()
+                                .getResources().getString(R.string.error), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                updateEmptyElert();
+                adapter.hideAllSelecting();
+
+                if (records.size() == 0 || records.isEmpty()) {
+                    editBtn.performClick();
+                }
+            }
+        });
+
+        playRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                play();
+            }
+        });
+
+        rewindRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pos <= 0) {
+                    pos = files.size() - 1;
+                } else {
+                    pos--;
+                }
+
+                initPlayer(pos);
+            }
+        });
+
+        forwardRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pos < files.size() - 1) {
+                    pos++;
+                } else {
+                    pos = 0;
+
+                }
+                initPlayer(pos);
             }
         });
 
@@ -165,7 +235,7 @@ public class DeletedRecordsActivity extends AppCompatActivity implements RecordA
         recentlyDeletedRecyclerView.setAdapter(adapter);
     }
 
-    private void showYesNoDialogForDelete() {
+    private void showYesNoDialog() {
         final Dialog dialog = new Dialog(DeletedRecordsActivity.this);
         dialog.setContentView(R.layout.dialog_overwrite_record);
         dialog.setCanceledOnTouchOutside(false);
@@ -221,59 +291,6 @@ public class DeletedRecordsActivity extends AppCompatActivity implements RecordA
         dialog.show();
     }
 
-    private void showYesNoDialogForRecover() {
-        final Dialog dialog = new Dialog(DeletedRecordsActivity.this);
-        dialog.setContentView(R.layout.dialog_overwrite_record);
-        dialog.setCanceledOnTouchOutside(false);
-
-        TextView title = dialog.findViewById(R.id.mainTitleDialogMakeChange);
-        TextView description = dialog.findViewById(R.id.descriptionDialogMakeChange);
-
-        title.setText(getResources().getString(R.string.recover_record));
-        description.setText(getResources().getString(R.string.recover_description));
-
-        Button no = dialog.findViewById(R.id.noDialogMakeChange);
-        Button yes = dialog.findViewById(R.id.yesDialogMakeChange);
-        yes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (recoverBtn.getText().equals(getResources().getString(R.string.recover_all))) {
-                    if (adapter.recoverAllRecords(allFiles, allRecords)) {
-                        Toast.makeText(view.getContext(), view.getContext()
-                                .getResources().getString(R.string.recover_all_alert), Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(view.getContext(), view.getContext()
-                                .getResources().getString(R.string.error), Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    if (adapter.recoverAllSelected(allFiles, allRecords)) {
-                        Toast.makeText(view.getContext(), view.getContext()
-                                .getResources().getString(R.string.recover_alert), Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(view.getContext(), view.getContext()
-                                .getResources().getString(R.string.error), Toast.LENGTH_SHORT).show();
-                    }
-                }
-                updateEmptyElert();
-                adapter.hideAllSelecting();
-
-                if (records.size() == 0 || records.isEmpty()) {
-                    editBtn.performClick();
-                }
-
-                dialog.dismiss();
-            }
-        });
-        no.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-            }
-        });
-
-        dialog.show();
-    }
-
     private void updateEmptyElert() {
         if (records.isEmpty()) {
             emptyAlert.setVisibility(View.VISIBLE);
@@ -284,6 +301,7 @@ public class DeletedRecordsActivity extends AppCompatActivity implements RecordA
 
     private void SetUp() {
         recentlyDeletedRecyclerView = findViewById(R.id.recentlyDeletedRecyclerView);
+        sharedPreferences = getSharedPreferences("setting", Context.MODE_PRIVATE);
 
         editBtn = findViewById(R.id.editInDelButton);
         recoverBtn = findViewById(R.id.recoverButton);
@@ -296,11 +314,177 @@ public class DeletedRecordsActivity extends AppCompatActivity implements RecordA
         emptyAlert = findViewById(R.id.emptyAlert);
 
         playHeading = findViewById(R.id.playHeading);
+        namePlayingRecord = findViewById(R.id.namePlayingRecord);
+
+        durationPlayingRecord = findViewById(R.id.durationPlayingRecord);
+        startTimePlayingRecord = findViewById(R.id.startTimePlayingRecord);
+        endTimePlayingRecord = findViewById(R.id.endTimePlayingRecord);
 
         utilLayout = findViewById(R.id.deleteUtilsLayout);
 
+        playRecord = findViewById(R.id.playRecord);
+        rewindRecord = findViewById(R.id.rewindRecord);
+        forwardRecord = findViewById(R.id.forwardRecord);
+
+        seekBarRecord = findViewById(R.id.seekBarRecord);
+        deletePlayingRecord = findViewById(R.id.deletePlayingRecord);
+
+
         updateDataRecords();
 
+    }
+
+    private void initPlayer(final int position) {
+        if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+            mMediaPlayer.reset();
+            seekBarRecord.setProgress(0);
+            String sTime = createTimeLabel(0);
+            startTimePlayingRecord.setText(sTime);
+            String eTime = createTimeLabel(mMediaPlayer.getDuration());
+            endTimePlayingRecord.setText(eTime);
+        }
+
+        String sname = files.get(position).getName().replace(".mp3", "").replace(".m4a", "").replace(".wav", "").replace(".m4b", "").replace(".3gp", "").replace("mp4", "");
+        namePlayingRecord.setText(sname);
+
+        mMediaPlayer = MediaPlayer.create(getApplicationContext(), Uri.parse(files.get(position).getPath())); // create and load mediaplayer with song resources
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                String totalTime = createTimeLabel(mMediaPlayer.getDuration());
+                durationPlayingRecord.setText(totalTime);
+                seekBarRecord.setMax(mMediaPlayer.getDuration());
+                mMediaPlayer.start();
+                playRecord.setBackgroundResource(R.drawable.red_pause);
+            }
+        });
+
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                // Continuous
+                if (sharedPreferences.getBoolean("continuous", false)) {
+                    int curSongPosition = position;
+                    // code to repeat songs until the
+                    if (curSongPosition < files.size() - 1) {
+                        curSongPosition++;
+                        initPlayer(curSongPosition);
+                    } else {
+                        curSongPosition = 0;
+                        initPlayer(curSongPosition);
+                    }
+                } else {
+                    releasePlaySession();
+                }
+
+            }
+        });
+
+        seekBarRecord.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mMediaPlayer.seekTo(progress);
+                    seekBarRecord.setProgress(progress);
+                    String sTime = createTimeLabel(progress);
+                    startTimePlayingRecord.setText(sTime);
+                    String eTime = createTimeLabel(mMediaPlayer.getDuration() - progress);
+                    endTimePlayingRecord.setText(eTime);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                if (mMediaPlayer.isPlaying())
+                    pause();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                if (!mMediaPlayer.isPlaying())
+                    play();
+            }
+        });
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                while (mMediaPlayer != null) {
+                    try {
+//                        Log.i("Thread ", "Thread Called");
+                        // create new message to send to handler
+                        if (mMediaPlayer.isPlaying()) {
+                            Thread.sleep(1000);
+                            Message msg = new Message();
+                            msg.what = mMediaPlayer.getCurrentPosition();
+                            handler.sendMessage(msg);
+
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+//            Log.i("handler ", "handler called");
+            int current_position = msg.what;
+
+            seekBarRecord.setProgress(current_position);
+            String sTime = createTimeLabel(current_position);
+            String eTime = createTimeLabel(mMediaPlayer.getDuration() - current_position);
+            startTimePlayingRecord.setText(sTime);
+            endTimePlayingRecord.setText(eTime);
+
+        }
+    };
+
+    private void play() {
+        if (mMediaPlayer != null && !mMediaPlayer.isPlaying()) {
+            mMediaPlayer.start();
+            playRecord.setBackgroundResource(R.drawable.red_pause);
+        } else {
+            pause();
+        }
+
+    }
+
+    private void pause() {
+        if (mMediaPlayer.isPlaying()) {
+            mMediaPlayer.pause();
+            playRecord.setBackgroundResource(R.drawable.red_play);
+        }
+
+    }
+
+    public String createTimeLabel(int duration) {
+        String timeLabel = "";
+        int min = duration / 1000 / 60;
+        int sec = duration / 1000 % 60;
+
+        timeLabel += min + ":";
+        if (sec < 10) timeLabel += "0";
+        timeLabel += sec;
+
+        return timeLabel;
+    }
+
+    private void releasePlaySession() {
+        playRecord.setBackgroundResource(R.drawable.red_play);
+        namePlayingRecord.setText(getResources().getString(R.string.record_name));
+        durationPlayingRecord.setText("00:00");
+        seekBarRecord.setMax(0);
+        startTimePlayingRecord.setText("00:00");
+        endTimePlayingRecord.setText("00:00");
+        mainPlayLayout.setVisibility(View.GONE);
     }
 
     private void updateDataRecords() {
@@ -343,5 +527,7 @@ public class DeletedRecordsActivity extends AppCompatActivity implements RecordA
 
     @Override
     public void onItemClick(int position) {
+        initPlayer(position);
+        pos = position;
     }
 }
